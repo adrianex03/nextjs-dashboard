69838100a4fab4a74fccb95eda2245bb7ebeb646
Adrian Rivera <adrianex03@icloud.com>
2024-03-26 01:50:06 -0400

    Up to chapter 8

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -3,7 +3,7 @@ import { Exo_2, Inter, Lusitana } from 'next/font/google';
 export const inter = Inter({ subsets: ['latin'] });
 
 export const exo_2 = Exo_2({
-    weight: ['300', '700'],
+    weight: ['300', '900'],
     subsets: ['latin'],
   });
   export const lusitana = Lusitana({

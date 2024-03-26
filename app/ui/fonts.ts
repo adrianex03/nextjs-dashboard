2b7567f39c5d7576feeee5c20c16c5c9b03b07fb
Adrian Rivera <adrianex03@icloud.com>
2024-03-25 22:48:20 -0400

    First Commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Exo_2, Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const exo_2 = Exo_2({
+    weight: ['300', '700'],
+    subsets: ['latin'],
+  });
+  export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
